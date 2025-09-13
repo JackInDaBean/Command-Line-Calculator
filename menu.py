b742a3417f34f827c509f5d6e63fd042c6a47a62
Jake Isaac Harvey <jake.harvey115@gmail.com>
2025-09-14 11:10:44 +1200

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,13 @@
+def display_menu_prompt():
+    """
+    Display the menu prompt for Command Line Calculator
+    
+    Returns
+        Nothing - only prints a message.
+    """
+    print("\nPlease select the calculation that you wish to perform:")
+    print("\n-------------------------------------------------------")
+    print("0 -- Square root")
+    print("1 -- Exponents")
+    print("2 -- Cube Root")
+    print("-------------------------------------------------------")
